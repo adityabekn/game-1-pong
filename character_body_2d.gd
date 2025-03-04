dd345fe04b04729e04d0c8e8fd0b1e9fd4ebd6fa
Aditya Bekti Nugroho <bekti.aditya16@gmail.com>
2025-03-04 15:53:23 +0700

    Add movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,11 +3,14 @@ extends Node2D
 @export var SPEED: float = 300.0
 
 func _process(delta: float):
-	var direction = 0;
+	var direction: int = 0;
 	if Input.is_action_pressed("ui_up"):
 		print("Up")
-		direction = 1;
+		direction = -1;
 			
 	if Input.is_action_pressed("ui_down"):
 		print("Down")
-		direction = -1;	
+		direction = 1;
+
+	# Apply movement
+	position.y += direction * SPEED * delta
